eaa15bf01a44f94469a4e3fb43cb74d895b043fd
Rone Clay <ronec1r550@gmail.com>
2024-06-22 20:46:34 -0300

    Initial Commit
    Implementação da estrutura de dados lista, utilizando Array

---
+++ src/List/Lista.java
@@ -0,0 +1,50 @@
+package List;
+
+public class Lista<T> {
+    private Object[] lista = new Object[100];
+    private int quantidadeObjetos =0;
+
+    public void adiciona(T objeto){
+        this.adicionaNaPosicao(this.quantidadeObjetos, objeto);
+    }
+    public T pega(int posicao){
+        if (lista[posicao] == null){
+            return null;
+        }
+        System.out.println("Objeto da posição "+posicao+": ");
+        return (T) lista[posicao];
+    }
+    public int tamanho(){
+        System.out.println("Tamanho da lista:");
+        return this.quantidadeObjetos;
+    }
+
+    public void remove(int posicao){
+        while(lista[posicao+1]!=null){
+            lista[posicao]=lista[posicao+1];
+            lista[posicao+1]=null;
+        }
+        this.quantidadeObjetos -=1;
+        System.out.println("Removido");
+    }
+    public void adicionaNaPosicao(int posicao, T objeto){
+        while (lista[posicao]!=null){
+            T proximoObjeto = (T) lista[posicao];
+            lista[posicao]=objeto;
+            objeto=proximoObjeto;
+            posicao+=1;
+        }
+        lista[posicao]=objeto;
+        this.quantidadeObjetos +=1;
+        System.out.println("Adicionado");
+    }
+
+    public void mostraLista(){
+        System.out.println("....");
+        for (int i=0; i< lista.length; i++){
+            if (lista[i]!=null){
+                System.out.println(lista[i]);
+            }
+        }
+    }
+}
